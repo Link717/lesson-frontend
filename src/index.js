c661a4091ac7a746acbc284f04dbd41590246863
Link717 <ksy95433@gmail.com>
2021-01-21 22:08:31 +0900

    Set: 초기 셋팅

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import Routes from "./Routes";
 
-ReactDOM.render(<Routes />, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById("root"));
